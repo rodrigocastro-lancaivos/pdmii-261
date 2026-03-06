4aa5ce0e33c5a1181802494fd8cf17322a56129d
rodrigocast <rodrigo.castro05@aluno.ifce.edu.br>
2026-03-06 09:57:26 -0300

    exercicio 01, questoes de 4 a 5 feitas

--- exercicios/exercicio-01/main.dart
+++ exercicios/exercicio-01/main.dart
@@ -13,4 +13,22 @@ void main() {
   double temperatura = 27;
 
   print("cidade $cidade com $idade2 anos a $temperatura graus e temperatura");
+
+  //questao-04
+  int a = 7;
+  int b = 3;
+
+  print("Soma de a + b: ${a+b}");
+  print("Subtração de a - b: ${a-b}");
+  print("Multiplicação de a * b: ${a*b}");
+  print("Divisão de a / b: ${a/b}");
+
+  //questao-05
+  String? apelido;
+
+  print("Antes apelido: $apelido");
+
+  apelido = "cuquita";
+
+  print("Depois apelido: $apelido");
 }
