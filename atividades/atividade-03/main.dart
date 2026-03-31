fa429f4788f17e95efb00988943f2d81f6f35df1
samuel douglas da silva gois <samuel.douglas06@aluno.ifce.edu.br>
2026-03-31 16:35:14 -0300

    atividade-03 Taveira concluida

--- atividades/atividade-03/main.dart
+++ atividades/atividade-03/main.dart
@@ -1,4 +1,4 @@
-// 14-agregacao.dart  
+// 14-agregacao.dart
 // Agregação e Composição
 import 'dart:convert';
 
@@ -8,6 +8,10 @@ class Dependente {
   Dependente(String nome) {
     this._nome = nome;
   }
+
+      Map<String, dynamic> toJson() => {
+        'nome': _nome
+      };
 }
 
 class Funcionario {
@@ -18,6 +22,11 @@ class Funcionario {
     this._nome = nome;
     this._dependentes = dependentes;
   }
+
+      Map<String, dynamic> toJson() => {
+        'nome': _nome,
+        'preco': _dependentes
+      };
 }
 
 class EquipeProjeto {
@@ -28,16 +37,34 @@ class EquipeProjeto {
     _nomeProjeto = nomeprojeto;
     _funcionarios = funcionarios;
   }
+
+    Map<String, dynamic> toJson() => {
+        'nome': _nomeProjeto,
+        'preco': _funcionarios
+      };
 }
 
 void main() {
   // 1. Criar varios objetos Dependentes
+  var dependente1 = Dependente('Vitor');
+  var dependente2 = Dependente('Gabriel');
+  var dependente3 = Dependente('Baiao');
+
   // 2. Criar varios objetos Funcionario
-  // 3. Associar os Dependentes criados aos respectivos
-  //    funcionarios
+
+  // 3. Associar os Dependentes criados aos respectivos funcionarios
+  var funcionario1 = Funcionario('Caio', [dependente1]);
+  var funcionario2 = Funcionario('Nicolas', [dependente2]);
+  var funcionario3 = Funcionario('Ravel', [dependente3, dependente1]);
+
   // 4. Criar uma lista de Funcionarios
-  // 5. criar um objeto Equipe Projeto chamando o metodo
-  //    contrutor que da nome ao projeto e insere uma
-  //    coleção de funcionario
+  List<Funcionario> timeAlpha = [funcionario1, funcionario2, funcionario3];
+
+  // 5. criar um objeto Equipe Projeto chamando o metodo contrutor que da nome ao projeto e insere uma coleção de funcionario
+  var projeto1 = EquipeProjeto('pdmii-261', timeAlpha);
+
   // 6. Printar no formato JSON o objeto Equipe Projeto.
+  String printJson = jsonEncode(projeto1);
+
+  print(printJson);
 }
